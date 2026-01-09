fd1e7cce0643d16a1a38d5779848a52647eb2db1
edwin <arayamaicol201277@gmail.com>
2026-01-09 16:08:48 -0600

    Print
    prueba

--- JavaApplication2/src/algoritmos/Main.java
+++ JavaApplication2/src/algoritmos/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Probando github");
+        System.out.println("uygvsedhujb");
     }
-    
+
 }
