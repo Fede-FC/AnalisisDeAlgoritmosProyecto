0c5accd813ed1ec8fe97ee8035b5ab073bc8e2e9
Fede-FC <federick.fernandez10@gmail.com>
2026-01-10 23:14:36 -0600

    Pieza y puzzle

---
+++ JavaApplication2/src/conceptos/Pieza.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conceptos;
+
+/**
+ *
+ * @author feder
+ */
+public class Pieza {
+    
+    public Pieza() {}
+    public Pieza(int top, int right, int bottom, int left, int id) {
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+        this.id = id;
+    }
+    
+    int top;
+    int right;
+    int bottom;
+    int left;
+    int id;          
+    
+    public void rotarIzq() { // mas 5 aasignaciones 
+        int aux = this.top;
+        this.top = this.right;
+        this.right = this.bottom;
+        this.bottom = this.left; 
+        this.left = aux;
+    }
+    public void rotarDer(){ // mas 5 aasignaciones 
+        int aux = this.top;
+        this.top = this.left;
+        this.left = this.bottom;
+        this.bottom = this.right;
+        this.right = aux;
+    }
+    
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) { // mas 1 asignacion
+        this.top = top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) { // mas 1 asignacion
+        this.right = right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) { // mas 1 asignacion
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) { // mas 1 asignacion
+        this.left = left;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { // mas 1 asignacion
+        this.id = id;
+    }
+    
+    
+}
---
+++ JavaApplication2/src/conceptos/Puzzle.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conceptos;
+
+/**
+ *
+ * @author feder
+ */
+public class Puzzle {
+    int size;
+    Pieza[][] tablero;
+
+    public Puzzle(int size) {
+        this.size = size;
+        this.tablero = new Pieza[size][size];
+        
+    }
+    
+    public void colocarPieza(int row, int col, Pieza pieza){
+        this.tablero[row][col] = pieza;
+    }
+    public void quitarPieza(int row, int col){
+        this.tablero[row][col] = null;
+    }
+    // Funcion que indica si el puzle esta completo :D
+    public boolean isComplet(){ // O(n!)
+        for (int i = 0; i < this.size; i++){
+            for (int j = 0; j < this.size; j++){
+                if (this.tablero[i][j] == null) return false;
+            }
+        }
+        return true; 
+    }
+    /*
+    Funcion que verifica si la pieza a colocar es valida (para verificar solucion).
+    verifica arriba y izquierda.
+    Derecha y abajo no ya que no sean crean todavia.
+    */
+    public boolean isValid(int row, int col, Pieza pieza){
+        // arriba
+        if (row > 0 && this.tablero[row - 1][col] != null) {
+            if (this.tablero[row - 1][col].getBottom() != pieza.getTop())
+                return false;
+        }
+        // izquierda
+        if (col > 0 && this.tablero[row][col - 1] != null){
+            if (this.tablero[row][col - 1].getRight() != pieza.getLeft())
+                return false;
+        }
+        return true;
+    }
+    
+    public void print() {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            System.out.print(this.tablero[i][j] == null ? "---- " : this.tablero[i][j] + " ");
+        }
+        System.out.println();
+    }
+}
+}
