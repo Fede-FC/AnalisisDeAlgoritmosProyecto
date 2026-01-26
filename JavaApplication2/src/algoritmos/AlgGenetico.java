81744eedf484b60e30ad42f430a1a7ba3d45634a
edwin <arayamaicol201277@gmail.com>
2026-01-25 21:02:00 -0600

    Update AlgGenetico.java

--- JavaApplication2/src/algoritmos/AlgGenetico.java
+++ JavaApplication2/src/algoritmos/AlgGenetico.java
@@ -30,190 +30,139 @@ public class AlgGenetico {
         } 
     }
     
-    
     public ArrayList<Puzzle> cruce(ArrayList<Puzzle> ancestros){
         Random random = new Random();
-        //int aleatorio = random.nextInt(this.tamano);
+        hijos.clear();
+        Random rand = new Random();
+        int listaPuzzles = this.tamano * this.tamano;
         
-        
-        
-        
-        while (hijos.size() < 2*poblacionInicial) {
-            Puzzle padre= ancestros.get(random.nextInt(this.tamano));
-            Puzzle madre= ancestros.get(random.nextInt(this.tamano));
+        while (hijos.size() < 2 * poblacionInicial) {
+            Puzzle padre = ancestros.get(random.nextInt(ancestros.size()));
+            Puzzle madre = ancestros.get(random.nextInt(ancestros.size()));
             
-            if (padre==madre) continue;
+            if (padre == madre) continue;
             
-            Puzzle pHijo =cruzarFilas(padre, madre);
-            hijos.add(pHijo);
-            pHijo =cruzarFilas(madre, padre);
-            hijos.add(pHijo);
+            int puntoA = rand.nextInt(listaPuzzles);
+            int puntoB = rand.nextInt(listaPuzzles);
+            hijos.add(crucePMX(padre, madre, puntoA, puntoB));
+            hijos.add(crucePMX(madre, padre, puntoA, puntoB));
         }
-        
-        
         return hijos;
     }
-    
-    public Puzzle cruzarFilas(Puzzle padre, Puzzle madre) {
-        Puzzle hijo = new Puzzle(tamano);
 
+    public Puzzle crucePMX(Puzzle padre, Puzzle madre, int puntoA, int puntoB) {
+        int totPiezas = tamano * tamano;
+        Pieza[] piezaP = new Pieza[totPiezas]; 
+        Pieza[] piezaM = new Pieza[totPiezas]; 
+        Pieza[] hijo = new Pieza[totPiezas];
+        Puzzle nuevo = new Puzzle(tamano);
+        
+        if (puntoA > puntoB) {
+            int t = puntoA;
+            puntoA = puntoB;
+            puntoB = t;
+        }        
+        
+        for (int i = 0; i < totPiezas; i++) {
+            
+            int row = i / tamano;
+            int column = i % tamano;
+            
+            piezaP[i] = padre.getPieza(row, column);
+            piezaM[i] = madre.getPieza(row, column);
+        }
 
-        for (int i = 0; i < tamano; i++) {
-            for (int j = 0; j < tamano; j++) {
-                if (i % 2 == 0) {
-                    Pieza pPadre= padre.getPieza(i, j);
+        
+        for (int posicion = puntoA; posicion <= puntoB; posicion++) {
+            hijo[posicion] = copiar(piezaP[posicion]);
+        }
 
-                    Pieza actualP = new Pieza(
-                        pPadre.getTop(), 
-                        pPadre.getRight(), 
-                        pPadre.getBottom(), 
-                        pPadre.getLeft(),
-                        pPadre.getId());
+        for (int posicion = puntoA; posicion <= puntoB; posicion++) {
+            int id = piezaM[posicion].getId();
 
-                    hijo.colocarPieza(i, j, actualP);
-                } else {
-                    Pieza pMadre= madre.getPieza(i, j);
+            if (!contiene(hijo, id)) {
+                int pos = posicion;
 
-                    Pieza actualM = new Pieza(
-                        pMadre.getTop(), 
-                        pMadre.getRight(), 
-                        pMadre.getBottom(), 
-                        pMadre.getLeft(),
-                        pMadre.getId());
-
-                    hijo.colocarPieza(i, j, actualM);
+                while (pos >= puntoA && pos <= puntoB) {
+                    int idMap = piezaP[pos].getId();
+                    pos = buscarPos(piezaM, idMap);
                 }
+
+                hijo[pos] = copiar(piezaM[posicion]);
             }
         }
-        return hijo;
-}
+
+        for (int i = 0; i < totPiezas; i++) {
+            if (hijo[i] == null) {
+                hijo[i] = copiar(piezaM[i]);
+            }
+        }
+
+        for (int i = 0; i < totPiezas; i++) {
+            int row = i / tamano;
+            int column = i % tamano;
+            nuevo.colocarPieza(row, column, hijo[i]);
+        }
+
+        return nuevo;
+    }
+
+ 
+    private boolean contiene(Pieza[] arr, int id) {
+        for (Pieza p : arr)
+            if (p != null && p.getId() == id) return true;
+        return false;
+    }
+
+    private int buscarPos(Pieza[] arr, int id) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i].getId() == id) return i;
+        return -1;
+    }
+
+    private Pieza copiar(Pieza p) {
+        return new Pieza(p.getTop(), p.getRight(), p.getBottom(), p.getLeft(), p.getId());
+    }
+
     public void mutarPoblacion() {
+        Random rand = new Random();
+
         for (int i = 0; i < puzzleList.size(); i++) {
-            Puzzle original = puzzleList.get(i);
-            Puzzle mutado = PuzzleFactory.copiarPuzzle(original);
+            Puzzle p = puzzleList.get(i);
 
-            int fitnessAntes = original.evaluateFitness();
-
-            boolean reparo = repararRepetidos(mutado);
-
-            if (!reparo) {
-                mutacionLeve(mutado);
-            }
-
-            int fitnessDespues = mutado.evaluateFitness();
-
-            if (fitnessDespues > fitnessAntes) {
-                puzzleList.set(i, mutado);
+            if (rand.nextDouble() < 0.2) { 
+                mutacionLeve(p);
             }
         }
     }
 
-    private boolean repararRepetidos(Puzzle p) {
-        int size = p.getSize();
-        int n = size * size;
-        boolean[] vistos = new boolean[n];
-        ArrayList<int[]> repetidas = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            int r = i / size;
-            int c = i % size;
-            Pieza pieza = p.getPieza(r, c);
-            if (pieza == null) continue;
-
-            int id = pieza.getId();
-            if (vistos[id]) {
-                repetidas.add(new int[]{r, c});
-            } else {
-                vistos[id] = true;
-            }
-        }
-
-        ArrayList<Integer> faltantes = new ArrayList<>();
-        for (int id = 0; id < n; id++) {
-            if (!vistos[id]) {
-                faltantes.add(id);
-            }
-        }
-
-        if (repetidas.isEmpty()) return false;
-
-        Pieza molde = null;
-        for (int i = 0; i < size && molde == null; i++) {
-            for (int j = 0; j < size; j++) {
-                Pieza tmp = p.getPieza(i, j);
-                if (tmp != null) {
-                    molde = tmp;
-                    break;
-                }
-            }
-        }
-
-        int idx = 0;
-        for (int[] pos : repetidas) {
-            int idFaltante = faltantes.get(idx++);
-
-            Pieza nueva = new Pieza(
-                    molde.getTop(),
-                    molde.getRight(),
-                    molde.getBottom(),
-                    molde.getLeft(),
-                    idFaltante
-            );
-
-            p.colocarPieza(pos[0], pos[1], nueva);
-        }
-
-        return true;
-    }
 
     private void mutacionLeve(Puzzle p) {
         Random rand = new Random();
         int size = p.getSize();
 
-        if (rand.nextBoolean()) {
-            int r1 = rand.nextInt(size);
-            int c1 = rand.nextInt(size);
-            int r2 = rand.nextInt(size);
-            int c2 = rand.nextInt(size);
+        int r1 = rand.nextInt(size);
+        int c1 = rand.nextInt(size);
+        int r2 = rand.nextInt(size);
+        int c2 = rand.nextInt(size);
 
-            Pieza a = p.getPieza(r1, c1);
-            Pieza b = p.getPieza(r2, c2);
+        
+        if (r1 == r2 && c1 == c2) return;
 
-            if (a != null && b != null) {
-                p.colocarPieza(r1, c1, b);
-                p.colocarPieza(r2, c2, a);
-            }
-        } else {
-            int r = rand.nextInt(size);
-            int c = rand.nextInt(size);
+        Pieza a = p.getPieza(r1, c1);
+        Pieza b = p.getPieza(r2, c2);
 
-            Pieza pieza = p.getPieza(r, c);
-            if (pieza != null) {
-                if (rand.nextBoolean()) pieza.rotarIzq();
-                else pieza.rotarDer();
-            }
+        if (a != null && b != null) {
+            p.colocarPieza(r1, c1, b);
+            p.colocarPieza(r2, c2, a);
         }
     }
 
 
-    private Pieza buscarPiezaPorId(Puzzle p, int id) {
-        int size = p.getSize();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (p.getPieza(i, j).getId() == id) {
-                    return p.getPieza(i, j);
-                }
-            }
-        }
-        return null;
-    }
-
-
-    
-    
     public void resolver(){
         ArrayList<Puzzle> hijos;
-        for (int generation = 0; generation < 20000; generation++) {
+        for (int generation = 0; generation < 100000; generation++) {
             //Ordenar
             Collections.sort(puzzleList, (a,b) -> b.evaluateFitness() - a.evaluateFitness());
             //Hijos=2*Poblacion inicial - BUA DE LOCOS
@@ -230,7 +179,7 @@ public class AlgGenetico {
             //Eliminar los de menos rendimiento, ya sea padres o hijos - (Parte completa)
             puzzleList.subList(poblacionInicial, puzzleList.size()).clear();
             
-            //Falta metodo mutar
+            //metodo para mutar a la poblacion :)
             mutarPoblacion();
 
         }
@@ -242,7 +191,7 @@ public class AlgGenetico {
     
     public static void main(String[] args) {
 
-        int size = 3;
+        int size = 10;
         int maxValue = 9;
 
         ArrayList<Puzzle> puzzleList = new ArrayList<>();
@@ -252,7 +201,7 @@ public class AlgGenetico {
         base.print();
         System.out.println("--------------------------------");
 
-        int poblacionInicial = 3;
+        int poblacionInicial = 30;
         if (size < 30) {
             poblacionInicial = size;
         }
